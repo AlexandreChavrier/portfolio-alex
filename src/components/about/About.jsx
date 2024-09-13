3ab304f89715531d6669c81b2cd8eaf06d0aab78
alexandre chavrier <alex69.chavrier@gmail.com>
2024-09-13 13:37:04 +0200

    final commit

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -13,11 +13,11 @@ function About() {
                 <img src={Avatar2} alt="" className="about__img" />
                 <div className="about__data grid">
                     <div className="about__info">
-                        <p className="about__description">  I'm Alexandre CHAVRIER, a <strong>junior full stack developper</strong> from Lyon, France. 
+                        <p className="about__description">I'm Alexandre CHAVRIER, a <strong>junior full stack developper</strong> from Lyon, France. 
                         I'm currently working at ada tech school for <strong>9 months</strong> and <strong>1 year on a sandwich course</strong>.<br />
-                            I'm looking for a <strong>work-study placement</strong> to put my development skills and rigor to good use in the tech world.<br />
-                            I'm good at <strong>algorithms</strong> and my favorite languages are <strong>JS</strong> and <strong>Python</strong>. I also really like <strong>Node/React</strong> projects where i can combine back-end and front-end skills.<br />
-                            I am motivated, disciplined, and eager to <strong>learn</strong> new technologies or <strong>refine</strong> those I already know. My ability to grasp complex concepts is one of my greatest <strong>strengths</strong>,
+                        I'm looking for a <strong>work-study placement</strong> to put my development skills and rigor to good use in the tech world.<br />
+                        I'm good at <strong>algorithms</strong> and my favorite languages are <strong>JS</strong> and <strong>Python</strong>. I also really like <strong>Node/React</strong> projects where i can combine back-end and front-end skills.<br />
+                        I am motivated, disciplined, and eager to <strong>learn</strong> new technologies or <strong>refine</strong> those I already know. My ability to grasp complex concepts is one of my greatest <strong>strengths</strong>,
                         which I developed during my previous studies in law.</p>
                         <a href="../../public/CV_Alexandre_Chavrier_F.pdf" className="btn" id='btn__download' download>Download CV</a>
                     </div>
